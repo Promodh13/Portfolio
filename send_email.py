f040235522780c2212178b98053b0030530ce993
Promodh <promodh2003@gmail.com>
2023-04-09 00:24:49 +0530

    Secure password in environment variable.

--- send_email.py
+++ send_email.py
@@ -1,18 +1,17 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "promodh2003@gmail.com"
-    password = "ljyaiqylvwibwkbs"
+    password = os.getenv("PASSWORD")
     receiver = "promodh2003@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-
-
 
