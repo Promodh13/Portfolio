a77e7f3451a34b7bd90da4b7034b7bdb5e0eec35
Promodh <promodh2003@gmail.com>
2023-04-08 11:16:22 +0530

    Add description on new column

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ with col2:
     st.title("Promodh")
     content = """Hi, I am Promodh. I am an undergraduate in IT from MIT.
     I am very much interested in Python and hoping to get more experience
-    in data science and excel in it.
+    in data science and excel in it. I have been currently studying 
+    web development using Python. I want to improve my knowledge in AI,ML,DS
+    like concepts and master it.
     """
     st.info(content)
+
+content2 = """Below you can find some of the apps I have built in Python.
+             Feel free to contact me."""
+st.subheader(content2)
+
 
